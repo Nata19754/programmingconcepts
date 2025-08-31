df7ea27b630fc9f1091f695abda8eff6a8f8bc2d
Nata19754 <chavesnatally0419@gmail.com>
2025-08-30 22:32:26 -0400

    new changes

---
+++ variables.py
@@ -0,0 +1,22 @@
+print("Input: 5 multiplied by 6")
+print("The answer to your query is", 5*6)
+print("-------------------------------------")
+
+print("Input: 8 divided by 3")
+print("The answer to your query is", 8/3)
+print("-------------------------------------")
+
+print("Input: 8 divided by 3 without the remainder")
+print("The answer to your query is", 8//3)
+print("-------------------------------------")
+
+print("Input:8 modulo 3 (remainder only)")
+print("The answer to your query is", 8%3)
+print("-------------------------------------")
+
+print("Input:4200000 add 23090")
+print("The answer to your query is", 4200000+23090)
+print("-------------------------------------")
+
+print("Input: 4200000 subtract 7633")
+print("The answer to your query is", 4200000-7633)
