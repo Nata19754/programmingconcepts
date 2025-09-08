d2d25abf14df60772ccc98c96f68f94d4a751908
Nata19754 <chavesnatally0419@gmail.com>
2025-09-07 22:13:38 -0400

    Added pseudocode

--- Branching.py
+++ Branching.py
@@ -2,12 +2,15 @@
 # 7.633 cents for the first 1000 hours used
 # 9.259 cents for any hours used over 1000 hours.
 
+# Named and assigned values to variables
 RATE_FIRST_1000 = 0.07633
 RATE_OVER_1000 = 0.09259
 THRESHOLD = 1000
 
+# Created infinite loop to ensure valid input is received from user
 while True:
     try:
+        #prompts user for input and converts it to a variable, which is stored in the kwh variable
         kwh = int(input(" Enter the KW hours used: "))
         if kwh > 0:
             break
